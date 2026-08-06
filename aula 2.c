bd3349e488917dc40e2016b2c1a796c54d946db5
tozojoao <tozojoao675@gmail.com>
2026-08-05 22:26:44 -0300

    Add files via upload

---
+++ aula 2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+
+	float a,b,c,media;
+
+   	a = 2;
+	b = 55;
+ 	c = 25;
+ 	
+ 	media = (a+b+c) / 3;
+
+	printf("a media é de %f",media);
+
+  return 0;
+  
+}
